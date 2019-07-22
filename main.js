300afeec42e1b40010a5ba22bd573c8fc9fb1886
Guillermo <guiboco18@gmail.com>
2019-07-22 21:10:00 +0200

    create portfolio

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let refran = document.querySelector('#refran');
+    document.addEventListener('scroll', () => {
+        if (window.pageYOffset > 400) {
+            refran.className = "slideIn";
+            refran.style.display = "inline-block";
+        }
+    });
+    window.onscroll = function () {
+        console.log(
+            'top: ' + (window.pageYOffset || document.documentElement.scrollTop) + ' ' +
+            'left: ' + (window.pageXOffset || document.documentElement.scrollLeft)
+        );
+    }
+})
